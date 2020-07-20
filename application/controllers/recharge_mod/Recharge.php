ecdd0c31555f716b547f7202993b0f101ed724b4
Navneet Singh <navneetofficial25@gmail.com>
2020-07-20 20:41:33 +0530

    changes in wallet

--- application/controllers/recharge_mod/Recharge.php
+++ application/controllers/recharge_mod/Recharge.php
@@ -4,18 +4,18 @@ class Recharge extends My_Controller {
 
     public function index()
     {
-       $this->load->model('Db_wallet');
+       $this->load->model('db_wallet');
         
-        $this->data['post'] = $this->Db_wallet->get_balance($this->session->userdata("id"));
+        $this->data['post'] = $this->db_wallet->get_balance($this->session->userdata("id"));
         $this->load->view('frontend/common/header', $this->data);
         $this->load->view('frontend/recharge', $this->data);
         $this->load->view('frontend/common/footer', $this->data);
     }
      function buyplan(){
-        $this->load->model('Db_Wallet');
+        $this->load->model('db_wallet');
         $wallet=$this->input->post('wallet');
         $update_bal=$this->input->post('balance')+1200;
-        if($this->Db_Wallet->update_wallet($wallet,$update_bal)){
+        if($this->db_wallet->update_wallet($wallet,$update_bal)){
             $this->session->set_flashdata('success',true); 
             redirect(base_url().'recharge_mod/recharge');
         }
