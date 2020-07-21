85f2a4a9a225c840fda6ef531e12b3f360f3ce1e
Anshu Sharma <mycodebook15@gmail.com>
2020-07-21 22:41:25 +0530

    contact page update

--- application/views/frontend/contact.php
+++ application/views/frontend/contact.php
@@ -1,4 +1,4 @@
-<style>
+<!-- <style>
     .post-requirment input {
         padding: 0;
         margin: 0;
@@ -28,19 +28,23 @@
     .post-card img {
         width: 3rem;
     }
-</style>
+</style> -->
+
+
+<!-- 
 <section class="py-5 px-3" style="background-color: #fafafa;">
+
     <div class="py-5 container text-center">
         <h3 class="pt-5"><span class="text-danger">Get In Touch with Us</span> - Free to ask any question from us</h3>
-        <h6>More than 10,000 Properties added daily</h6>
+        <h6>We are here to help you there</h6>
     </div>
+
     <div style="border: 1px solid #ccc;" class="container form-padding">
         <div class="row">
             <div style="background-color: #fff;" class="p-4 col-md-9">
                 <h5 class="text-black">Contact Us <span class="text-danger">*</span> </h5>
                 <form class="post-requirment my-3">
                     <div class="form-group">
-                        <!-- <label for="inputAddress">Address</label> -->
                         <input type="email" class="form-control" id="inputAddress" placeholder="Your name *">
                     </div>
                     <div class="form-group">
@@ -85,7 +89,58 @@
             </div>
         </div>
     </div>
+</section> -->
+
+<main class="sub-main">
+    <section class="getintouch">
+        <div class="container">
+            <div class="getintouch_head text-center">
+                <h1 class="getintouch_h1">Get In Touch</h1>
+            </div>
+        </div>
+    </section>
+<section class="contact">
+    <div class="container">
+        <div class="contact_bx">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="contact_head">
+                        <h2>Contact Us</h2>
+                        <p>Get In touch with us and Thank you for reaching out and wanting to know us!</p>
+                    </div>
+                    <div class="form-group">
+                        <form action="">
+                            <label for=""></label>
+                            <input type="text" name="" id="" class=" contact_inp" placeholder="Your Name" aria-describedby="helpId">
+                            <input type="email" name="" id="" class=" contact_inp" placeholder="Your Email" aria-describedby="helpId">
+                            <input type="number" name="" id="" class=" contact_inp" placeholder="Your Mobile" aria-describedby="helpId">
+                            <textarea name="" id="" cols="30" rows="10" placeholder="Your Message" class=" contact_inp"></textarea>
+                            <div class="contact_btn_bx">
+                                <button type="submit" class="btn btn-primary contact_btn">Send</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="contact_side">
+                        <div class="contact_side_top">
+                            <h2 class="contact_side_top_h1">We are here to Help you out</h2>
+                            <p>The Contact Us page is one of the most important pages on your website. Get inspired by these 25 amazing examples,</p>
+                        </div>
+                        <div class="contact_side_top">
+                            <h2 class="contact_side_top_h1">We are here to Help you out</h2>
+                            <p>The Contact Us page is one of the most important pages on your website. Get inspired by these 25 amazing examples,</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
+</main>
+
+
+
 
 <script>
     function myFunction() {
