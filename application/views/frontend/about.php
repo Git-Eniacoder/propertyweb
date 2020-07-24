55e3cde018e920d16438312d6dcde326eae2a0e6
Anshu Sharma <mycodebook15@gmail.com>
2020-07-24 16:01:45 +0530

    about us page and policy

--- application/views/frontend/about.php
+++ application/views/frontend/about.php
@@ -13,7 +13,7 @@
             <div class="about-box">
                 <div class="row justify-content-center">
                     <div class="col about-head">
-
+                        <img src="" alt="">
                     </div>
                 </div>
 
@@ -40,7 +40,9 @@
                             <div class="about_subhead">
                                 <h2>About the<span class="about-head-h2"> Company</span></h2>
                             </div>
-                            <p>Students are tomorrow's leaders" our mission is to provide spiritual growth,mental and physical growth in today's world.True education is complete education. Right or complete education can produce ideal individuals.</p>
+                            <p>Hello Rent India is a concept of B2B (Business to Business) and BC (Business to Customer).We are specifically focusing on Rented Accommodation which is easily available in India through online portal. </p>
+                            <p>The story started when I visited the Metro City last year searching for rented accommodation for one of my relatives for job purposes. He got the rented premises after facing lot of problems. After that, I decided to work on such portal with a huge potential wherein anyone can visit the online portal and search for rented accommodation with minimum efforts creating trust amongst them. </p>
+                            <p>We have also planned to launch "Recharge Portal" within this portal to increase our "Hello Rent India" visibility amongst users. </p>
                         </div>
                     </div>
                     <!-- <div class="col-md-5">
@@ -59,7 +61,8 @@
                             <div class="about_subhead">
                                 <h2>Our <span class="about-head-h2"> Mission</span></h2>
                             </div>
-                            <p>Students are tomorrow's leaders" our mission is to provide spiritual growth,mental and physical growth in today's world.True education is complete education. Right or complete education can produce ideal individuals.</p>
+                            <p>Hello Rent India is specifically focusing on Rented Accommodation property which is easily available in India through its online portal. </p>
+                            <p>Our mission is the business model we follow by striving to rent out listed properties on our portal for as long as possible with chosen services to our clients - are what continues to ensure its success.</p>
                         </div>
 
                     </div>
@@ -67,11 +70,12 @@
 
                 <div class="row justify-content-center about_contain">
                     <div class="col-md-7">
-                    <div class="about_text text-center">
+                        <div class="about_text text-center">
                             <div class="about_subhead">
                                 <h2>Our <span class="about-head-h2"> Vision</span></h2>
                             </div>
-                            <p>Students are tomorrow's leaders" our mission is to provide spiritual growth,mental and physical growth in today's world.True education is complete education. Right or complete education can produce ideal individuals.</p>
+                            <p>Hello Rent India is specifically focusing on Rented Accommodation property which is easily available in India through its online portal. </p>
+                            <p>Our vision is we want to be positively recognized as an innovative and growing company by the business model we follow through our listed properties on our portal. We will continue to make our mark in new and original ways through our modern business concepts.</p>
                         </div>
                     </div>
                     <!-- <div class="col-md-5">
@@ -79,6 +83,41 @@
                        
 
                     </div> -->
+                </div>
+                <div class="row justify-content-center about_contain">
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body">
+                                <h3 class="about_mentor">Sh. Radha Kishan Kaushik</h3>
+                                <p><span> <em>Founder</em> </span></p>
+                                <p class="card-text">Some quick example text to build on the card.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body">
+                                <h3 class="about_mentor">Anoopnandan Kaushik</h3>
+                                <p><span> <em>CEO</em> </span></p>
+                                <p class="card-text">Some quick example text to build on the card.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body">
+                                <h3 class="about_mentor">Sh. Radha Kishan Kaushik</h3>
+                                <p><span> <em>CEO</em> </span></p>
+                                <p class="card-text">Some quick example text to build on the card.</p>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
 
---
+++ application/views/frontend/policy.php
@@ -0,0 +1,36 @@
+<main class="sub-main policy">
+    <section class="getintouch">
+        <div class="container">
+            <div class="getintouch_head text-center">
+                <h1 class="getintouch_h1">Privacy Policy</h1>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="about">
+        <div class="container">
+            <div class="about-box">
+                <div class="row justify-content-center">
+                    <div class="col-10 policy_page">
+                        <div class="policy_page_head">
+                            <h2>Hello Rent India Privacy Policy</h2>
+                        </div>
+                        <div class="policy_page_content">
+                            <p>Not everyone knows how to make a Privacy Policy agreement, especially with CCPA or GDPR or CalOPPA or PIPEDA or Australia's Privacy Act provisions. If you are not a lawyer or someone who is familiar to Privacy Policies, you will be clueless. Some people might even take advantage of you because of this. Some people may even extort money from you. These are some examples that we want to stop from happening to you.
+
+                                We will help you protect yourself by generating a Privacy Policy.
+
+                                Our Privacy Policy Generator can help you make sure that your business complies with the law. We are here to help you protect your business, yourself and your customers.
+
+                                Fill in the blank spaces below and we will create a personalized website Privacy Policy for your business. No account registration required. Simply generate & download a Privacy Policy in seconds!
+
+                                Small remark when filling in this Privacy Policy generator: Not all parts of this Privacy Policy might be applicable to your website. When there are parts that are not applicable, these can be removed. Optional elements can be selected in step 2. The accuracy of the generated Privacy Policy on this website is not legally binding. Use at your own risk.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+</main>
