10350c478b6e517512cb3ffd2d863e7d9a795e7d
himanshu goyal <himanshugoyal1011@gmail.com>
2020-07-24 23:25:27 +0530

    all page are linked

---
+++ application/views/frontend/common/wallet-header.php
@@ -0,0 +1,114 @@
+<html>
+   <head>
+      <title>Wallet Recharge</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+      <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+      <link rel="stylesheet" href="<?php echo $url['stylecss']; ?>">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+      
+   </head>
+   <body>
+      <header class="header-area bg-white meu ">
+         <div class="navbar-area ">
+            <div id="btn">
+               <div id='top'></div>
+               <div id='middle'></div>
+               <div id='bottom'></div>
+            </div>
+            <div id="box">
+               <div id="items">
+                  <div class="item"><a href="recharge"><img src="../assets/img/Mob.svg" width="12px" style="margin-top:-5px; margin-right: 5px;"> Recharge & PayBills</a></div>
+                  <div class="item"><img src="../assets/img/buyplan.svg" width="20px" style="margin-top:-5px; margin-right: 5px;">Buy Plan</div>
+                  <div class="item"><a href="refer"><img src="../assets/img/refer.svg" width="20px" style="margin-right: 5px;">Refer And Earn</a></div>
+                  <div class="item"><a href="field"><img src="../assets/img/Field Expense.png" width="20px"style="margin-right: 5px;" > Field Expeses</a></div>
+                  <div class="item"><a href="addmoney"><img src="../assets/img/Add-Money.svg" width="20px"style="margin-right: 5px;" >Add Money</a></div>
+                  <hr class="style1">
+                  
+                  <div class="item"><a href="<?php echo $url['support']; ?>"  ><i class="fa fa-envelope" style="color: white;" aria-hidden="true" width="20px"style="margin-right: 5px;"></i> Suppoprt</a></div>
+                  <div class="item"><a ><img src="../assets/img/about.svg" width="20px" style="margin-top:-5px; margin-right: 5px;">  About us</a></div>
+                  <hr class="style1">
+                  <div style="padding-left: 1rem; color:grey; font-size:1.3rem;" >connect & share on</div>
+                  <div style="padding-left: 1rem;">
+                  <a href="#" class="ic fa fa-facebook"></a>
+                  <a href="#" class="ic fa fa-instagram"></a>
+                  <a href="#" class="ic fa fa-youtube"></a>
+                  <a href="#" class="ic fa fa-twitter"></a>
+                  </div>
+                  
+                  
+               </div>
+            </div>
+            <div class="container">
+               <div class="row">
+                  <div class="col-lg-12">
+                     <nav class="navbar navbar-expand-lg">
+                        <div class="logo lo-mm">
+                           <a class="custom-logo-link "  href="<?php echo base_url(); ?>">
+                           <img width="133" height=30 src="<?php echo $url['image'] . 'logo/' . $site['site_logo'] ?>" alt="Logo" class="custom-logo">
+                           </a>
+                        </div>
+                        <div id="navbarSupportedContent" class="collapse navbar-collapse sub-menu-bar">
+                           <ul id="nav" class="navbar-nav ml-auto ">
+                              <li class=" nav-item">
+                                 <a style="color: rgb( 245, 57, 61 );" title="Refer and Earn" href="<?php echo $url['referandearn']; ?>" class="nav-link">
+                                 Refer and Earn
+                                 </a>
+                              </li>
+                              <li   class="nav-item m-1 brk ">
+                                 |
+                              </li>
+                              <li class=" nav-item">
+                                 <a title="Balance" href="#" class="nav-link">
+                                <img src="../assets/img/Balance.png" width="25px">  Balance <i class='fas fa-coins'></i> <?php echo $post["all_data"][0]->recharge_wallet;?>.00
+                                 </a>
+                              </li>
+                              <li class=" nav-item">
+                                 <a title="Balance" href="#" class="nav-link">
+                                <img src="../assets/img/Balance.png" width="25px">  Level : <?php echo $level[0];?>
+                                 </a>
+                              </li>
+                              <li   class="  nav-item m-1 brk ">
+                                 |
+                              </li>
+                              <li class="mx-2 nav-item">
+                                 <div class="dropdown user-flag">
+                                    <img style=" margin-top: .3rem;" src="../assets/img/flag.png" width="30px" class=" dropbtn" >
+                                    <span class="caret"></span>
+                                    <div class="dropdown-content">
+                                       <a class="dropdown-item" href="#">India</a>
+                                       <a class="dropdown-item" href="#">Nepal</a>
+                                    </div>
+                                 </div>
+                              </li>
+                              <li   class="  nav-item m-1 brk ">
+                                 |
+                              </li>
+                              <li class="nav-item">
+                                 <div class="dropdown user-fa">
+                                 <i class="fa fa-user" aria-hidden="true"></i>
+                                 <span class="caret"></span>
+                                    <div class="dropdown-content">
+                                    <a class="dropdown-item" href="<?php echo $url['wallet']; ?>">Wallet</a>
+                                     <a class="dropdown-item" href="<?php echo $url['support']; ?>">Raise Ticket</a>
+                                     <a class="dropdown-item" href="<?php echo base_url() . 'login/logout'; ?>">Logout</a>
+                                    </div>
+                                 </div>
+                              </li>
+                           </ul>
+                        </div>
+                     </nav>
+                     <!-- navbar -->
+                  </div>
+               </div>
+               <!-- row -->
+            </div>
+            <!-- container -->
+         </div>
+         <!-- navbar area -->
+      </header>
+      
--- application/views/frontend/wallet/addmoney.php
+++ application/views/frontend/wallet/addmoney.php
@@ -1,157 +1,89 @@
-<html>
-    <head>
-        <title>Wallet Recharge</title>
-
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<style>
+.add-money{
+  width: 100%;
+  height: auto;
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+.add-money .card{
+  width: 100%;
+  height: auto;
+  padding-top: 1rem;
+  border: 1px solid rgba(233, 227, 227, 0.836);
+}
+.add-money .card h4{
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-weight: 500;
+  text-align: center;
+  color: rgb( 245, 57, 61 );
+}
+.add-money .card button{
+  width: 100%;
+  height: auto;
+  padding-top: .5rem;
+  padding-bottom: .5rem;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid rgba(233, 227, 227, 0.836);
+  outline: none;
+  background-color: white;
+  color: grey;
+  margin-top: 0px;
+  transition: .5s;
+}
+.add-money .card button:hover{
+  border-bottom: 1px solid rgb( 245, 57, 61 );
   
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="<?php echo $url['stylecss']; ?>">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        <style>
-            .add-money{
-                width: 100%;
-                height: auto;
-                padding-top: 3rem;
-                padding-bottom: 3rem;
-            }
-            .add-money .card{
-                width: 100%;
-                height: auto;
-                padding-top: 1rem;
-                border: 1px solid rgba(233, 227, 227, 0.836);
-            }
-            .add-money .card h4{
-                font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-                font-weight: 500;
-                text-align: center;
-                color: rgb( 245, 57, 61 );
-            }
-            .add-money .card button{
-                width: 100%;
-                height: auto;
-                padding-top: .5rem;
-                padding-bottom: .5rem;
-                border-top: none;
-                border-left: none;
-                border-right: none;
-                border-bottom: 1px solid rgba(233, 227, 227, 0.836);
-                outline: none;
-                background-color: white;
-                color: grey;
-                margin-top: 0px;
-                transition: .5s;
-            }
-            .add-money .card button:hover{
-                border-bottom: 1px solid rgb( 245, 57, 61 );
-                
-            }
-            
-        </style>    
-    
-    </head>
-    <body>
-    <header class="header-area bg-white meu ">
-         <div class="navbar-area ">
-            <div id="btn">
-               <div id='top'></div>
-               <div id='middle'></div>
-               <div id='bottom'></div>
-            </div>
-            <div id="box">
-               <div id="items">
-                  <div class="item"><i class="fa fa-mobile" aria-hidden="true"></i> Recharge & PayBills</div>
-                  <div class="item">Buy Plan</div>
-                  <div class="item"><img src="../assets/img/refer.svg" width="20px">Refer And Earn</div>
-                  <div class="item"><img src="../assets/img/Field Expense.png" width="20px"> Field Expeses</div>
-                  <div class="item"><img src="../assets/img/Add-Money.svg" width="20px">Add Money</div>
-                  <hr class="style1">
-                  
-                  <a href="<?php echo $url['support']; ?>" class="item"><i class="fa fa-envelope" aria-hidden="true"></i> Suppoprt</a>
-                  <div class="item">About us</div>
-                  <hr class="style1">
-                  <div style="padding-left: 1rem; color:grey; font-size:1.3rem;" >connect & share on</div>
-                  <div style="padding-left: 1rem;">
-                  <a href="#" class="ic fa fa-facebook"></a>
-                  <a href="#" class="ic fa fa-instagram"></a>
-                  <a href="#" class="ic fa fa-youtube"></a>
-                  <a href="#" class="ic fa fa-twitter"></a>
-                  </div>
-                  
-                  
-               </div>
-            </div>
-            <div class="container">
-               <div class="row">
-                  <div class="col-lg-12">
-                     <nav class="navbar navbar-expand-lg">
-                        <div class="logo lo-mm">
-                           <a class="custom-logo-link "  href="<?php echo base_url(); ?>">
-                           <img width="133" height=30 src="<?php echo $url['image'] . 'logo/' . $site['site_logo'] ?>" alt="Logo" class="custom-logo">
-                           </a>
-                        </div>
-                        <div id="navbarSupportedContent" class="collapse navbar-collapse sub-menu-bar">
-                           <ul id="nav" class="navbar-nav ml-auto ">
-                              <li class=" nav-item">
-                                 <a style="color: rgb( 245, 57, 61 );" title="Refer and Earn" href="<?php echo $url['referandearn']; ?>" class="nav-link">
-                                 Refer and Earn
-                                 </a>
-                              </li>
-                              <li   class="nav-item m-1 brk ">
-                                 |
-                              </li>
-                              <li class=" nav-item">
-                                 <a title="Balance" href="#" class="nav-link">
-                                <img src="../assets/img/Balance.png" width="25px">  Balance &#x20B9; <?php echo $post["all_data"][0]->recharge_wallet;?>.00
-                                 </a>
-                              </li>
-                              <li class=" nav-item">
-                                 <a title="Balance" href="#" class="nav-link">
-                                <img src="../assets/img/Balance.png" width="25px">  Level : <?php echo $level[0];?>
-                                 </a>
-                              </li>
-                              <li   class="  nav-item m-1 brk ">
-                                 |
-                              </li>
-                              <li class="mx-2 nav-item">
-                                 <div class="dropdown user-flag">
-                                    <img style=" margin-top: .3rem;" src="../assets/img/flag.png" width="30px" class=" dropbtn" >
-                                    <span class="caret"></span>
-                                    <div class="dropdown-content">
-                                       <a class="dropdown-item" href="#">India</a>
-                                       <a class="dropdown-item" href="#">Nepal</a>
-                                    </div>
-                                 </div>
-                              </li>
-                              <li   class="  nav-item m-1 brk ">
-                                 |
-                              </li>
-                              <li class="nav-item">
-                                 <div class="dropdown user-fa">
-                                 <i class="fa fa-user" aria-hidden="true"></i>
-                                 <span class="caret"></span>
-                                    <div class="dropdown-content">
-                                    <a class="dropdown-item" href="<?php echo $url['wallet']; ?>">Wallet</a>
-                                     <a class="dropdown-item" href="<?php echo $url['support']; ?>">Raise Ticket</a>
-                                     <a class="dropdown-item" href="<?php echo base_url() . 'login/logout'; ?>">Logout</a>
-                                    </div>
-                                 </div>
-                              </li>
-                           </ul>
-                        </div>
-                     </nav>
-                     <!-- navbar -->
-                  </div>
-               </div>
-               <!-- row -->
-            </div>
-            <!-- container -->
-         </div>
-         <!-- navbar area -->
-      </header>
+}
+.ic {
+  padding: 10px;
+  font-size: 20px;
+  width: 40px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50%;
+}
+
+.ic:hover {
+    opacity: 0.7;
+}
+
+ .fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+.fa-facebook:hover{
+  background: #3B5998;
+  color: white;
+}
+
+ .fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+.fa-twitter:hover {
+  background: #55ACEE;
+  color: white;
+}
+ .fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+.fa-youtube:hover {
+  background: #bb0000;
+  color: white;
+}
+ .fa-instagram {
+  background: #125688;
+  color: white;
+}
+.fa-instagram:hover {
+  background: #125688;
+  color: white;
+}
+</style>   
       <div class="wallet-op">
          <div class="container">
             <div class="row">
@@ -212,7 +144,7 @@
            </div> 
         </div> 
         </div>  
-   </body>  
+  
    <script>
       var sidebarBox = document.querySelector('#box');
       var sidebarBtn = document.querySelector('#btn');
@@ -245,4 +177,4 @@
               }
       });
    </script>    
-</html>
+
