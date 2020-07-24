d40934f8647e27f706280736e6b15a014bc4652d
Navneet Singh <navneetofficial25@gmail.com>
2020-07-24 18:59:07 +0530

    wallet changes

--- application/controllers/wallet/Refer.php
+++ application/controllers/wallet/Refer.php
@@ -21,8 +21,58 @@ class Refer extends My_Controller {
     {
        // $this->load->model('Db_wallet');
        // $this->data['post'] = $this->Db_wallet->get_balance($this->session->userdata("id"));
-        
+       $this->load->model('Db_wallet');
+       $this->data['post'] = $this->Db_wallet->get_balance($this->session->userdata("user_id"));
+       $this->data['level']=$this->find_level_and_points($this->data['post']["all_data"][0]->total_referal);
         $this->load->view('frontend/wallet/refer',$this->data);
         $this->load->view('frontend/common/footer',$this->data);
+    }
+    public function find_level_and_points($total_count){
+        if($total_count<pow(6,1)){
+            if($total_count==0)
+                return array(1,50,0);
+            if($total_count==1)
+                return array(1,70,0);
+            if($total_count==2)
+                return array(1,90,0);
+            if($total_count==3)
+                return array(1,110,0);
+            if($total_count==4)
+                return array(1,130,0);
+            if($total_count==5)
+                return array(1,150,0);
+            
+        }
+        if($total_count<pow(6,2)){
+            return array(2,900,0);
+        }
+        if($total_count<pow(6,3)){
+            if($total_count==pow(6,2))
+            {
+                return array(3,4320,10000);
+            }
+            return array(3,4320,0);
+        }
+        if($total_count<pow(6,4)){
+            if($total_count==pow(6,3))
+            {
+                return array(4,12960,25000);
+            }
+            return array(4,12960,0);
+        }
+        if($total_count<pow(6,5)){
+            if($total_count==pow(6,4))
+            {
+                return array(5,38880,50000);
+            }
+            return array(5,38880,0);
+        }
+        if($total_count<pow(6,6)){
+            if($total_count==pow(6,5))
+            {
+                return array(6,233280,75000);
+            }
+            return array(6,233280,0);
+        }
     }
 }   
