d73cf6499888fb39faed34ec4131ad77f73c7e01
Anshu Sharma <mycodebook15@gmail.com>
2020-07-24 13:26:38 +0530

    about page

---
+++ application/views/frontend/about.php
@@ -0,0 +1,23 @@
+<main class="sub-main">
+    <section class="getintouch">
+        <div class="container">
+            <div class="getintouch_head text-center">
+                <h1 class="getintouch_h1">About Us</h1>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="about">
+        <div class="container">
+            <div class="about-box">
+                <div class="row">
+                    <div class="col justify-content-center">
+                        <h2>New Beginning starts</h2>
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+    </section>
+</main>
