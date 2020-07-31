ff27b2bfd24debb36ab050716124f2b0db64d00d
himanshu goyal <himanshugoyal1011@gmail.com>
2020-07-31 15:42:49 +0530

    sidebar added on home page

--- application/views/frontend/common/header.php
+++ application/views/frontend/common/header.php
@@ -15,6 +15,55 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="<?php echo $url['stylecss']; ?>">
   <link rel="stylesheet" href="<?php echo $url['newhomecss']; ?>">
+
+  <style>
+  
+ 
+
+
+  .sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  right: 0px;
+  background-color: white;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: rgb( 245, 57, 61 );
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  left:0px;
+  font-size: 36px;
+  
+
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+
+
+
+  </style>  
 </head>
 
 <body>
@@ -125,11 +174,15 @@
             </div>
 
             <div class="wal_nav_list_right">
-              <div class="nav_list_icons nav_bar_click">
-                <span><i class="fa fa-bars" aria-hidden="true"></i></span>
-              </div>
+              <span style="font-size:30px;cursor:pointer;" onclick="openNav()">&#9776;</span>
             </div>
-
+            <div id="mySidenav" class="sidenav">
+              <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+              <a href="#">About</a>
+              <a href="#">Services</a>
+              <a href="#">Clients</a>
+              <a href="#">Contact</a>
+            </div>
           </div>
         </div>
 
@@ -264,3 +317,13 @@
       </div>
     </div>
   </div>
+
+  <script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+  </script>  
